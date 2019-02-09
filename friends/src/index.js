f3f26ee264d102d12fb553f41a8cc4ab40e56c44
fbt328 <katherine.rust.andrews@gmail.com>
2019-02-08 23:21:15 -0800

    initial reducer setup

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducer from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer, applyMiddleware(thunk, logger));
+
+
 ReactDOM.render(
     <Provider store = {store}>
         <App />
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,48 @@
+// action imports here
+import { FRIENDS_GET } from '../actions' /*grabbing friend data */
+import { FRIENDS_POST } from '../actions' /*creating new friend */
+import { FRIENDS_SUCCESS } from '../actions'
+import { FRIENDS_FAIL } from '../actions' /*error message */
+
+const initialState = {
+    friends: [],
+    fetchingFriends: false,
+    creatingFriends: false,
+    error: null
+}
+
+export const friendsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FRIENDS_GET:
+            return {...state, fetchingFriends: true};
+        case FRIENDS_POST:
+            return {...state, creatingFriends: true};
+        case FRIENDS_SUCCESS:
+            return {...state, fetchingFriends: false, friends:[...state.friends, ...action.payload]};    
+        case FRIENDS_FAIL:
+            return {...state, error:action.payload};
+        default:
+            return state; 
+    }
+}
+
+
+// example initialState from project info
+// const initialState = {
+//     fetchingFriends: false,
+//     friendsFetched: false,
+//     friendsSaved: false,
+//     savingFriends: false,
+//     updatingFriend: false,
+//     friendUpdated: false,
+//     deletingFriend: false,
+//     friendDeleted: false,
+//     friends: [],
+//     error: null
+// }
+// Each friend item that is in the friends array should have the following format:
+// {
+//   name: 'Luis',
+//   age: 24,
+//   email: 'luis@lambdaschool.com',
+// }
